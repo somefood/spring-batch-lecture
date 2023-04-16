5dace93728db9907752fda4397d6c876801a5083
Seokju Hong <iamseokjuhong@gmail.com>
2023-04-16 19:40:52 +0900

    프로젝트 구성 및 의존성 설정

--- src/main/java/io/springbatch/springbatchlecture/SpringBatchLectureApplication.java
+++ src/main/java/io/springbatch/springbatchlecture/SpringBatchLectureApplication.java
@@ -1,9 +1,11 @@
 package io.springbatch.springbatchlecture;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableBatchProcessing // 스프링 배치가 작동하기 위해 선언
 public class SpringBatchLectureApplication {
 
     public static void main(String[] args) {
