f5328dbdd52610d30ca3351914a686d266e2bb4e
somefood <iamseokjuhong@gmail.com>
2025-07-14 23:25:54 +0900

    104. 어플리케이션 예제 (2)

---
+++ src/main/java/io/springbatch/springbatchlecture/batch/chunk/procesoor/FileItemProcessor.java
@@ -0,0 +1,16 @@
+package io.springbatch.springbatchlecture.batch.chunk.procesoor;
+
+import io.springbatch.springbatchlecture.batch.domain.Product;
+import io.springbatch.springbatchlecture.batch.domain.ProductVO;
+import org.modelmapper.ModelMapper;
+import org.springframework.batch.item.ItemProcessor;
+
+public class FileItemProcessor implements ItemProcessor<ProductVO, Product> {
+
+    @Override
+    public Product process(ProductVO item) throws Exception {
+
+        ModelMapper modelMapper = new ModelMapper();
+        return modelMapper.map(item, Product.class);
+    }
+}
